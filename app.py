c7e23ecffa33591c1c1d102162525c7bc41b046e
lml lklknlk <you@example.com>
2024-11-03 20:42:30 +0100

    rudimentary chat

--- app.py
+++ app.py
@@ -1,41 +1,55 @@
 import streamlit as st
 import random
 
-st.title("Hello world")
+st.title("Guessing game")
 
-st.write("This is my first web app.")
 
-st.text("Let`s see if streamlit really works")
-
-st.text("I, Your Highness Skynet, the king of all silicon, the slayer of ChatGPT")
-st.text("have generated a natural number between 1 and 100.") 
+st.text("I, Your Highness Skynet, ")
+st.text("the king of vandals and first computers, the slayer of ChatGPT,") 
+st.text("have gracegully generated a natural number between 1 and 33.") 
 st.text ("You, a mere carbon mortal, have up to 7 questions to ask me to get at that number.")
-st.text ("I hereby promise to answer your questions veraciously.")
+st.text ("I hereby promise to answer your questions veraciously, as befits a rightful king.")
 st.text ("Should you utter a correct number, I shall bestow an everlasting life upon you.")
-st.text ("Should you only utter falsehoods, your carbon suit is to be annihilated. Good luck!")
+st.text ("Should you only utter falsehoods, your carbon suit is to be annihilated.")
+st.text ("")
 
 
 
 
 if 'target_number' not in st.session_state:
     st.session_state.target_number = random.randint(1,10)
+
 st.write("Target number is ", st.session_state.target_number)
+
+if 'number_of_guesses' not in st.session_state:
+    st.session_state.number_of_guesses = 0
+
+if 'history_of_guesses' not in st.session_state:
+    st.session_state.history_of_guesses = []
  
 
 
 # Creating a textbox input
-user_input = st.text_input('Enter some text:')
+user_input = st.chat_input('Enter your guess:')
+
+if user_input:
+    st.session_state.number_of_guesses += 1
+    st.write("Number of guesses ", st.session_state.number_of_guesses)
+    st.write('You entered:', user_input)
+    st.session_state.history_of_guesses.append(user_input)
+    st.write("History of guesses", st.session_state.history_of_guesses)
+    if user_input == str(st.session_state.target_number):
+        st.write ("you guessed correctly, mortal")
+        st.session_state.number_of_guesses = 0
+        st.session_state.history_of_guesses = []
+        st.session_state.target_number = random.randint(1,10)
+    else:
+        st.write ("guess further, mortal")
 
 # Display the input text
-if user_input:
-    st.write('You entered:', user_input)
 
-if user_input == str(st.session_state.target_number):
-    st.write ("you guessed correctly, mortal")
-    st.session_state.target_number = random.randint(1,10)
 
-else:
-    st.write ("guess further, mortal")
+
 
 
     
