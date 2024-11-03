38b60d54c9bc14dae6236b453f7f13ff162b350d
lml lklknlk <you@example.com>
2024-11-03 09:57:11 +0100

    added a game setting

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ st.write("This is my first web app.")
 
 st.text("Let`s see if streamlit really works")
 
-st.text("I, Your Highness Skynet, the king of all silicon, the slayer of ChatGPT, have generated a natural number between 1 and 100.") 
-st.text ("You, a mere carbon mortal, have up to seven questions to aks me to get at that number.")
+st.text("I, Your Highness Skynet, the king of all silicon, the slayer of ChatGPT")
+st.text("have generated a natural number between 1 and 100.") 
+st.text ("You, a mere carbon mortal, have up to seven questions to ask me to get at that number.")
 st.text ("I hereby promise to answer your questions veraciously.")
 st.text ("Should you utter a correct number, I shall bestow an everlasting life upon you.")
 st.text ("Should you only utter falsehoods, your carbon suit is to be annihilated. Good luck!")
