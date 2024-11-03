b8ac02ec6f8dd745ef9c33d05caaf4fe2d2697c9
lml lklknlk <you@example.com>
2024-11-03 10:25:38 +0100

    closer to a game

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 
 st.title("Hello world")
 
@@ -8,10 +9,13 @@ st.text("Let`s see if streamlit really works")
 
 st.text("I, Your Highness Skynet, the king of all silicon, the slayer of ChatGPT")
 st.text("have generated a natural number between 1 and 100.") 
-st.text ("You, a mere carbon mortal, have up to seven questions to ask me to get at that number.")
+st.text ("You, a mere carbon mortal, have up to 7 questions to ask me to get at that number.")
 st.text ("I hereby promise to answer your questions veraciously.")
 st.text ("Should you utter a correct number, I shall bestow an everlasting life upon you.")
 st.text ("Should you only utter falsehoods, your carbon suit is to be annihilated. Good luck!")
+
+target_number = random.randint(1,10)
+st.write("Target number is ", target_number)
 
 
 # Creating a textbox input
@@ -20,3 +24,8 @@ user_input = st.text_input('Enter some text:')
 # Display the input text
 if user_input:
     st.write('You entered:', user_input)
+
+if user_input == target_number:
+    st.write ("you guessed correctly, mortal")
+else:
+    st.write ("guess further, mortal")
