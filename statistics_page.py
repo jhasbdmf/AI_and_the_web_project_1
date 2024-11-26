8b59d21bbb0f16dc24e63b7b88fd0eb445f3b820
lml lklknlk <you@example.com>
2024-11-26 21:21:42 +0100

    feat: guess evaluation might be there, but too slow to check

--- statistics_page.py
+++ statistics_page.py
@@ -3,26 +3,38 @@ import pandas as pd
 from openai import OpenAI
 
 
-
-
 def evaluate_quality_of_a_guess_via_openAI(prompt):
     client = OpenAI(api_key="sk-svcacct-sasYDh93HtW8T-ZtXNCUElcOwmpB__D0ql2JJXLPl3kTrwrVeY2W_hTXl1AhYMsT3BlbkFJLcW4LbU2SOAgDFeOFXJyCA-l_xvOKYqPDTy1YJ2lGsEqPHLIXGwctBw7FOuGVAA")
-    model = "gpt-4o-mini"
+  
 
     chat_completion = client.chat.completions.create(
-                model=model,
+                model="gpt-4o-mini",
                 messages=[
                     {"role": "user", "content": prompt},
                     ],
             )
-    quality_of_guess = chat_completion.choices[0].message.content.split(' ', 1)[0]
-    explanation = chat_completion.choices[0].message.content.split(' ', 1)[1]
-    return quality_of_guess, explanation
+    
+    return chat_completion.choices[0].message.content
 
-def generate_a_guess_quality_evalutaion_prompt(guess, target_species_name_to_guess):
-    prompt = ""
- 
-    return prompt
+
+
+def find_out_whether_a_guess_applies_to_a_genus(genus, guess):
+    prompt = "Does the statement {" + guess + "} apply to the genus " + genus + "? "
+    prompt += "If yes, return 1. Otherwise return 0. Return nothing beying 1 or 0. "
+    response = evaluate_quality_of_a_guess_via_openAI(prompt) 
+    st.write (response)
+    return response
+
+#iterate through all genera and check whether a guess is true of the currently interated genus
+def estimate_quality_of_a_guess(guess):
+    #genera_to_which_a_guess_applies = st.session_state.genera_frequency_count["Genera"].apply(find_out_whether_a_guess_applies_to_a_genus, args = (guess,))
+    #number_of_species_to_which_a_guess_applies = genera_to_which_a_guess_applies.dot(st.session_state.genera_frequency_count["Frequencies"])
+    classes_to_which_a_guess_applies = st.session_state.classes_frequency_count["Classes"].apply(find_out_whether_a_guess_applies_to_a_genus, args = (guess,))
+    number_of_species_to_which_a_guess_applies = classes_to_which_a_guess_applies.dot(st.session_state.classes_frequency_count["Frequencies"])
+    
+    quality_of_a_guess = number_of_species_to_which_a_guess_applies / len(st.session_state.st.session_state.species_info_dataframe)
+    return quality_of_a_guess
+
 
 st.title("Statistics")
 
@@ -41,6 +53,14 @@ if 'total_number_of_guesses_per_game_iteration' in st.session_state and len(st.s
     # Create the bar chart
     st.bar_chart(number_of_guesses_per_game_iteration)
 
+if "species_info_dataframe" in st.session_state and "genera_frequency_count" not in st.session_state:
+    st.session_state.genera_frequency_count = st.session_state.species_info_dataframe.iloc[:, 2].value_counts().reset_index()
+    st.session_state.genera_frequency_count.columns = ["Genera","Frequencies"]
+    st.session_state.classes_frequency_count = st.session_state.species_info_dataframe.iloc[:, 3].value_counts().reset_index()
+    st.session_state.classes_frequency_count.columns = ["Classes","Frequencies"]
+    #st.write(st.session_state.genera_frequency_count)
+    st.write(st.session_state.classes_frequency_count)
+
 
 if "history_of_guesses" in st.session_state:
     #for i in st.session_state.history_of_guesses:
@@ -51,5 +71,13 @@ if "history_of_guesses" in st.session_state:
     broader_info_on_guesses = pd.DataFrame(st.session_state.history_of_guesses, columns=[
         'Guess', 'Correct species name', 'Truth value of a guess', 'Number of guesses in this game iteration'
     ])
-    st.write(broader_info_on_guesses)
 
+    
+    st.write(broader_info_on_guesses)
+    #st.write(broader_info_on_guesses['Guess'].apply(find_out_whether_a_guess_applies_to_a_genus, args = ("Oryx",)))
+    st.write(broader_info_on_guesses['Guess'].apply(estimate_quality_of_a_guess))
+    
+
+
+
+    
--- strlit/Scripts/activate.bat
+++ strlit/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Python_projects\AI_web_project1\sl
+set VIRTUAL_ENV=D:\Python_projects\AI_and_the_web_project_1\strlit
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(sl) %PROMPT%
+set PROMPT=(strlit) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(sl) 
+set VIRTUAL_ENV_PROMPT=(strlit) 
 
 :END
 if defined _OLD_CODEPAGE (
