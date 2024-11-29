b478121a9876cc54db3035c0bba214ee4579b5ad
lml lklknlk <you@example.com>
2024-11-29 16:26:44 +0100

    comments, it works more or less

--- animal_guessing_game_page.py
+++ animal_guessing_game_page.py
@@ -1,48 +1,133 @@
 import streamlit as st
-import random
 from openai import OpenAI
 import pandas as pd
+import random
+import math
+import re
 
 
+#this method generates a gpt-4o-mini reponse to a prompt with a desired temperature/creativity and response length
+#afaik possible values of temperature are between [0, 2] where 2 would generate weird nonsense and 0 would give an uncreative but exact response
 
-def generate_a_prompt(guess, that_which_should_be_guessed):
-
-    prompt = "Imagine that you play an animal guessing game against a human. The vernacular species name a human should guess is: " + str(that_which_should_be_guessed[0]).upper() + ". "
-    prompt += "Keep in mind that the scientific name of that species is " + str(that_which_should_be_guessed[1]) + " " + str(that_which_should_be_guessed[2]) + ". "
-    prompt += "Higher taxa under which " + str(that_which_should_be_guessed[0]).upper() + " falls are " + str(that_which_should_be_guessed[3]) + ", " + str(that_which_should_be_guessed[4]) + " and " + str(that_which_should_be_guessed[5] + ". ") 
-    prompt += "The question or the guess of a human trying to get at " + str(that_which_should_be_guessed[0]).upper() + " is: " + guess + ". "
-    prompt += "Generate an explicit and honest response whether the question after the colon mentions " + str(that_which_should_be_guessed[0]).upper() + " or its synonym. "
-    prompt += "If the question after the colon does not mention " + str(that_which_should_be_guessed[0]).upper() + " or its synonym, then just respond to that guess or question. "
-    prompt += "Your response must NOT contain the expression " + str(that_which_should_be_guessed[0]).upper() + ", any of its constituent words or synonyms. "  
-    
-    prompt += "Your response must be without quotation marks. " 
-    prompt += "If the question or guess contains several species names, then at least one of them is to be compared with the target species name. "            
-    prompt += "The response should be made as if it was from a polite artificial superintelligence which somewhat looks down upon humans yet answers their questions directly, honestly and ironically. "
-                
-    prompt += "The response should include a funny, original and not overused joke about humans which does not mention " + str(that_which_should_be_guessed[0]) + "or its synonyms. "
-    prompt += "If you think that " + str(that_which_should_be_guessed[0]).split(maxsplit=-1)[-1] + " or its synonym is contained in the expression " + guess.upper() + ", then append you response with the number 1. "
-    prompt += "Otherwise append your response with the number 0. "
-    return prompt
-
-
-
-
-def generate_response_via_openAI(prompt):
-   
+def generate_a_response_via_openAI(prompt, desired_temperature=1.1, max_response_length=200):
+    #this one generates a response with given parameters
     chat_completion = st.session_state.client.chat.completions.create(
                 model="gpt-4o-mini",
                 messages=[
-                    {"role": "user", "content": prompt},
-                    ],
+                    {"role": "user", 
+                     "content": prompt
+                    }
+                ],
+                temperature=desired_temperature,  
+                max_tokens=max_response_length
             )
-    response = chat_completion.choices[0].message.content[:-1]
-    if chat_completion.choices[0].message.content[-1] == "1":
-        return response, True
-    else:
-        return response, False 
+    #this one returns the best response option gpt provides to the prompt given parameters
+    return chat_completion.choices[0].message.content
 
+#this method checks whether user input via a chat is the target species to be guessed
+def find_out_whether_guess_contains_target_species(guess, that_which_should_be_guessed):
+    #this checks if the user gussed the common name ofthe target species
+    prompt = "Does the guess contain the common name of the target species? "
+    prompt += "The common mame of target species is called { " + str(that_which_should_be_guessed[0]).split(maxsplit=-1)[-1] + "}. "
+    prompt += "The guess is {" + guess + "}. "
+    prompt += "If the guess contains the common name of the target species, return 1. Otherwise return 0."
+    response = generate_a_response_via_openAI(prompt, 0.001, 1)
+    if response == "1":
+        return True
+    #this checks if the user gussed the scientific name ofthe target species
+    prompt = "Does the guess contain the scientific name of the target species? "
+    prompt += "The scientific name of that species is {" + str(that_which_should_be_guessed[1]) + " " + str(that_which_should_be_guessed[2]) + "}. "
+    prompt += "The guess is {" + guess + "}. "
+    prompt += "If the guess contains the scientific name of the target species, return 1. Otherwise return 0."
+    response = generate_a_response_via_openAI(prompt, 0.001, 1)
+    if response == "1":
+        return True
+    #if the user guessed neither the common nor the scientific name of the species, then his guess is incorrect
+    return False
 
+#this method given feedback to the user depending on whether he guessed the target species
+def generate_a_response_to_a_guess(guess, that_which_should_be_guessed, guess_is_correct):
     
+    prompt = "Imagine that you, an artificial superintelligence, play an animal guessing game against a human. "
+    if guess_is_correct:
+        prompt += "The human made a correct guess. Inform the human about it. "
+    else:
+        prompt += "The human made an incorrect guess. Inform the human about it. "
+        prompt += "Also give a human a hint as to whether the guess applies to the target species. "
+        prompt += "The guess is {" + guess + "}. "
+        prompt += "The target species is called { " + str(that_which_should_be_guessed[0]).split(maxsplit=-1)[-1] + "}. "
+        prompt += "Do not mention the target species in your response"
+    prompt += "The response should be made as if it was from a polite artificial superintelligence which somewhat looks down upon humans yet answers their questions directly, honestly and ironically. "
+    response = generate_a_response_via_openAI(prompt)
+
+
+    return response
+
+
+
+
+
+
+#Once probability of a guess is know, this method returns entropy of a guess. 
+#Entropy of a guess is the metric I chose to quantify quality or goodness of a guess.
+#If a guess eliminates roughly a half of the species irrespective of whether it is correct or not, then it is a good guess,
+#because one can use several such guesses to arrive at the target species quickly irrespective of what the target species is.
+#If a guess only eliminates a couple of species when correct or incorrect, then one might need lots of such guesses to be
+#guaranteed to arrivate at the target species      
+def compute_entropy_of_a_guess(probability_of_a_guess):
+    if probability_of_a_guess == 0:
+        return 1
+    else:
+        shannon_entropy = -probability_of_a_guess*math.log(probability_of_a_guess, 2) - (1-probability_of_a_guess)*math.log(1-probability_of_a_guess, 2)
+        return shannon_entropy
+
+# This GPT-authored function checks if a string is numeric.
+def is_numeric(value):
+    return bool(re.match(r'^\d+(\.\d+)?$', str(value)))
+
+#this one evaluates quality of a guess with a help of the following methods
+#is_numeric, compute_entropy_of_a_guess, generate_a_response_via_openAI
+
+def estimate_quality_of_a_guess(guess):
+    #one gets all taxonomic classes from which a species can be sampled
+    relevant_chordate_classes = st.session_state.species_info_dataframe["Class"].unique()
+    #this list will store the number of species in each class as estimated by gpt
+    number_of_species_of_a_given_class = []
+    #this list will store the number of species in each class of which a guess is true as estimated by gpt
+    number_of_species_of_a_given_class_of_which_a_guess_is_true = []
+    
+    #this iterates over all relevant taxonomic classes to find out of the number
+    #of species in each class of which a guess is true and the number of species in those classes
+    for i in relevant_chordate_classes:
+        #this one lets gpt estimate the number of species in each class
+        prompt = "What is the number of " + i + " species? "
+        prompt += " As a response give me just one token: the number of those species and nothing more. "
+        openAI_response = generate_a_response_via_openAI(prompt, 0.01, 1)
+        if not is_numeric(openAI_response):
+            openAI_response = "0"
+        number_of_species_of_a_given_class.append (float(openAI_response))
+
+        #this one lets gpt estimate the number of species in each class of which the guess is true
+        prompt =  "What is the number of " + i + " species of which the following guess is true? "
+        prompt += "The guess is: {" + guess + "} "
+        prompt += " As a response give me just one token: the number of those species and nothing more. "
+        openAI_response = generate_a_response_via_openAI(prompt, 0.01, 1)
+        if not is_numeric(openAI_response):
+            openAI_response = "0"
+        number_of_species_of_a_given_class_of_which_a_guess_is_true.append(float(openAI_response))
+    
+    #sanity check: number of species in a class of which a guess is true should not exceed the number of species in that class
+    for i in range (min(len(number_of_species_of_a_given_class), len(number_of_species_of_a_given_class_of_which_a_guess_is_true))):
+        number_of_species_of_a_given_class_of_which_a_guess_is_true[i] = min(number_of_species_of_a_given_class_of_which_a_guess_is_true[i], number_of_species_of_a_given_class[i])
+
+    #probability of a guess is the number of species in all relevant classes of which a guess is true divided into
+    #the total number of species in those classes
+    probability_of_a_guess = sum(number_of_species_of_a_given_class_of_which_a_guess_is_true) / sum (number_of_species_of_a_given_class)
+    #compute the entropy of a guess from its probability
+    entropy_of_a_guess = compute_entropy_of_a_guess(probability_of_a_guess)
+    #st.write("prob, ent ", probability_of_a_guess, entropy_of_a_guess)
+
+    return entropy_of_a_guess
 
 
 
@@ -63,87 +148,124 @@ st.text("""
         Should you only utter falsehoods, your carbon suits will perish eventually.     
 """)
 
+#initialize session variables
+
+#this one initializes gpt
 if "client" not in st.session_state:
     st.session_state.client = OpenAI(api_key="sk-svcacct-sasYDh93HtW8T-ZtXNCUElcOwmpB__D0ql2JJXLPl3kTrwrVeY2W_hTXl1AhYMsT3BlbkFJLcW4LbU2SOAgDFeOFXJyCA-l_xvOKYqPDTy1YJ2lGsEqPHLIXGwctBw7FOuGVAA")
 
 
+#this one reads vertebrate species of relevant classes from a preprocessed .csv file
+#each row of a .csv file contains
+#common name of a species
+#scientific name of a species (species and genus)
+#family, order, class and phylum of that species
+#7 columns in total 
 if "species_info_dataframe" not in st.session_state:
     filtered_chordates = pd.read_csv("filtered_chordate_species.csv", encoding='ISO-8859-1')
+    #remove duplicates as judged by the common name of a species
     filtered_chordates.drop_duplicates (subset=['Common Name'], inplace=True, ignore_index=True)
+    #remove duplicates as judged by the scientific name of a species (species + genus)
     filtered_chordates.drop_duplicates (subset=['Species', 'Genus'], inplace=True, ignore_index=True)
+    #sort entries by 3 columns: class, order, family, so that taxonomic relatives are close
+    #to one another is the table (was convenient for me while testing)
     filtered_chordates = filtered_chordates.sort_values(by=['Class', 'Order', 'Family'], ignore_index=True)
+    #store info on species in a session variable
     st.session_state.species_info_dataframe = filtered_chordates
 
+#total number of species in a dataset
 number_of_species = len(st.session_state.species_info_dataframe)
 
 if "current_target_species_number" not in st.session_state:
+    #generate a random number which would correspond to a row in a species_info_dataframe
     st.session_state.current_target_species_number = random.randint(0, number_of_species - 1)
 
 if "current_target_species_name" not in st.session_state:
+    #get info on a target species to guess from a species dataframe given the value of a random variable current_target_species_number
     st.session_state.current_target_species_name = st.session_state.species_info_dataframe.iat[st.session_state.current_target_species_number, 0]
 
-st.write (st.session_state.current_target_species_name)
+#st.write("Target number is ", st.session_state.current_target_species_number)
 
-
-#if 'target_number' not in st.session_state:
-#    st.session_state.target_number = random.randint(1,10)
-
-st.write("Target number is ", st.session_state.current_target_species_number)
-
+#this info will be displayed on the statistics page, so store in a session
 if 'current_number_of_guesses_in_current_game_iteration' not in st.session_state:
     st.session_state.current_number_of_guesses_in_current_game_iteration = 0
 
+#this info will be displayed on the statistics page, so store in a session
 if 'total_number_of_guesses_per_game_iteration' not in st.session_state:
     st.session_state.total_number_of_guesses_per_game_iteration = []
 
-
+#this will contain all relevant statistics for each of the guesses
 if 'history_of_guesses' not in st.session_state:
     st.session_state.history_of_guesses = []
 
+#this keeps track of chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+if "chat_input_enabled" not in st.session_state:
+    st.session_state.chat_input_enabled = True
 
-#st.dataframe(st.session_state.species_info_dataframe)
 
+
+#pictures for the user and the machine
 skynet_avatar_url = 'https://www.yourteesindy.com/cdn/shop/products/Skynet_1024x1024.png'
 user_avatar_url = "https://cdna.artstation.com/p/assets/images/images/004/943/296/large/andrey-pankov-neo.jpg?1487365474"
 
+#display chat history
 for message in st.session_state.messages:
     with st.chat_message(message["role"], avatar=message["avatar"]):
         st.markdown(message["content"])
 
-# Display the custom avatar
-#st.image(avatar_url, width=40)  # Adjust width as needed
 
-# Creating a textbox input
+
+
+
+#give the user a chance to enter a guess
 user_input = st.chat_input('Enter your question:')
 
 
-
+#if the user enterd something, then
 if user_input:
+   
+    st.session_state.chat_input_enabled = False
+    #there is one guess more in a current game iteration 
     st.session_state.current_number_of_guesses_in_current_game_iteration += 1
     
-
+    #push user input into message history to display it in futher runs of the page
     st.session_state.messages.append({"role": "user", "content": user_input, "avatar": user_avatar_url})
     
     
+    #display user input in the current run of the page
     with st.chat_message("user", avatar = user_avatar_url):
         st.write(user_input)
 
-    prompt = generate_a_prompt (user_input, st.session_state.species_info_dataframe.iloc[st.session_state.current_target_species_number])
-    #st.write(prompt)
-    response, user_input_contains_current_target_species = generate_response_via_openAI(prompt)
-
-    st.session_state.messages.append({"role": "Skynet", "content": response, "avatar": skynet_avatar_url})
+   
+    #check if user guessed the target species correctly
+    user_input_contains_current_target_species = find_out_whether_guess_contains_target_species(user_input, st.session_state.species_info_dataframe.iloc[st.session_state.current_target_species_number])
+  
+    #let gpt generate a response to user input
+    response_to_a_guess = generate_a_response_to_a_guess(user_input, st.session_state.species_info_dataframe.iloc[st.session_state.current_target_species_number], user_input_contains_current_target_species)
+    #push the generated response to user input into message history to display it in futher runs of the page
+    st.session_state.messages.append({"role": "Skynet", "content": response_to_a_guess, "avatar": skynet_avatar_url})
+    #display the generated response to the user input
     with st.chat_message("Skynet", avatar=skynet_avatar_url):
-            st.write(response)
-    
-    st.session_state.history_of_guesses.append([user_input, st.session_state.current_target_species_name, user_input_contains_current_target_species, st.session_state.current_number_of_guesses_in_current_game_iteration])
-    
+            st.write(response_to_a_guess)
+
+    #let gpt estimate the entropy and hence also quality of user guess/input
+    quality_of_a_guess = estimate_quality_of_a_guess(user_input)
+    #st.write("guess quality", quality_of_a_guess)
+    #save info on user guess/input, target species name, whether guess is correct, number of that guess in that game iteration, entropy/quality of that guess 
+    st.session_state.history_of_guesses.append([user_input, st.session_state.current_target_species_name, user_input_contains_current_target_species, st.session_state.current_number_of_guesses_in_current_game_iteration, quality_of_a_guess])
+    st.session_state.chat_input_enabled = True
+
+    #if user guessed target species correctly
     if user_input_contains_current_target_species:
+        #that the number of that guess in that game iteration is the total number of guesses in that game iteration
         st.session_state.total_number_of_guesses_per_game_iteration.append(st.session_state.current_number_of_guesses_in_current_game_iteration)
+        #counter of the number of guesses in a game iteration is to be set to 0
         st.session_state.current_number_of_guesses_in_current_game_iteration = 0
+        #show the user that he succeded in that game iteration
         st.balloons()
+        #sample a new species to guess from a species_info_dataframe
         st.session_state.current_target_species_number = random.randint(0, number_of_species - 1)
         st.session_state.current_target_species_name = st.session_state.species_info_dataframe.iat[st.session_state.current_target_species_number, 0]
--- statistics_page.py
+++ statistics_page.py
@@ -5,145 +5,46 @@ import math
 import re
 
 
-#def evaluate_quality_of_a_guess_via_openAI(prompt):
-def generate_a_response_via_openAI(prompt):
-
-    chat_completion = st.session_state.client.chat.completions.create(
-                model="gpt-4o-mini",
-                messages=[
-                    {"role": "user", 
-                     "content": prompt
-                    }
-                ],
-                temperature=0.01,  
-                max_tokens=1
-            )
-    return chat_completion.choices[0].message.content
-
-def compute_entropy_of_a_guess(probability_of_a_guess):
-    if probability_of_a_guess == 0:
-        return 1
-    else:
-        shannon_entropy = -probability_of_a_guess*math.log(probability_of_a_guess, 2) - (1-probability_of_a_guess)*math.log(1-probability_of_a_guess, 2)
-        return shannon_entropy
-
-# This GPT-authored function checks if a string is numeric.
-def is_numeric(value):
-    return bool(re.match(r'^\d+(\.\d+)?$', str(value)))
-
-def estimate_quality_of_a_guess(guess):
-    relevant_chordate_classes = st.session_state.species_info_dataframe["Class"].unique()
-    number_of_species_of_a_given_class = []
-    number_of_species_of_a_given_class_of_which_a_guess_is_true = []
-    
-    for i in relevant_chordate_classes:
-        prompt = "What is the number of " + i + " species? "
-        prompt += " As a response give me just one token: the number of those species and nothing more. "
-        openAI_response = generate_a_response_via_openAI(prompt)
-        if not is_numeric(openAI_response):
-            openAI_response = "0"
-        number_of_species_of_a_given_class.append (float(openAI_response))
-
-        prompt =  "What is the number of " + i + " species of which the following guess is true? "
-        prompt += "The guess is: {" + guess + "} "
-        prompt += " As a response give me just one token: the number of those species and nothing more. "
-        openAI_response = generate_a_response_via_openAI(prompt)
-        if not is_numeric(openAI_response):
-            openAI_response = "0"
-        number_of_species_of_a_given_class_of_which_a_guess_is_true.append(float(openAI_response))
-    
-    #sanity check: number of species in a class of which a guess is true should not exceed the number of species in that class
-    for i in range (min(len(number_of_species_of_a_given_class), len(number_of_species_of_a_given_class_of_which_a_guess_is_true))):
-        number_of_species_of_a_given_class_of_which_a_guess_is_true[i] = min(number_of_species_of_a_given_class_of_which_a_guess_is_true[i], number_of_species_of_a_given_class[i])
-
-    probability_of_a_guess = sum(number_of_species_of_a_given_class_of_which_a_guess_is_true) / sum (number_of_species_of_a_given_class)
-    entropy_of_a_guess = compute_entropy_of_a_guess(probability_of_a_guess)
-    st.write("prob, ent ", probability_of_a_guess, entropy_of_a_guess)
-    df = pd.DataFrame()
-    df["Classes"] = relevant_chordate_classes
-    df["Number_of_species"] = number_of_species_of_a_given_class
-    df["Guess_is_true"] = number_of_species_of_a_given_class_of_which_a_guess_is_true
-    #st.write(df)
-    #st.write(number_of_species_of_a_given_class)
-    #st.write(number_of_species_of_a_given_class_of_which_a_guess_is_true)
-    return entropy_of_a_guess
-    
-
-def estimate_quality_of_a_guess_in_a_simple_way(guess):
-    #get how classes of vertebrates and how many of those there are
-    relevant_chordate_classes = st.session_state.species_info_dataframe["Class"].value_counts()
-    
-    
-   
-   
-    
-
-    number_of_species_of_a_given_class_of_which_a_guess_is_true = []
-    for i in relevant_chordate_classes.to_dict():
-    #for i in relevant_chordate_classes.iloc[:, 0]:
-    #for i in value_counts_df.iloc[:, 0]:
-        prompt = ""
-        prompt += "What is the number of " + i + " species of which the following prompt is true. "
-        prompt += "The prompt is: {" + guess + "} "
-        prompt += " As a response give me just one token: the number of those species and nothing more. "
-        openAI_response = generate_a_response_via_openAI(prompt)
-        if not is_numeric(openAI_response):
-            openAI_response = "2"
-        number_of_species_of_a_given_class_of_which_a_guess_is_true.append(openAI_response)
-        #st.write(i, generate_a_response_via_openAI(prompt))
-
-       # relevant_chordate_classes["Count of species of which a guess is true"].append(generate_a_response_via_openAI(prompt))
-    
-    value_counts_df = st.session_state.species_info_dataframe.groupby('Class').size().reset_index(name='Count')
-    value_counts_df["guess_is_true"] = number_of_species_of_a_given_class_of_which_a_guess_is_true
-    value_counts_df['Count'] = pd.to_numeric(value_counts_df['Count'], errors='coerce')
-    value_counts_df['guess_is_true'] = pd.to_numeric(value_counts_df['guess_is_true'], errors='coerce')
-    st.write(value_counts_df)
-    number_of_species_of_which_a_guess_is_true = value_counts_df['Count'].dot(value_counts_df["guess_is_true"])
-    st.write(number_of_species_of_which_a_guess_is_true)
-    #probability_of_a_guess = int(number_of_species_of_which_a_guess_is_true)/len(st.session_state.species_info_dataframe)
-    #retutn (compute_entropy_of_a_guess(probability_of_a_guess))
-
-    
-
-    #prompt = "What is the percentage of mammals of which the following prompt is true. "
-    #prompt += "The prompt is: {" + guess + "} "
-    #prompt += " As a response give me just one token: percentage as a numeric value between 0 and 1 and nothing more "
-    #probability_of_a_guess = generate_a_response_via_openAI(prompt)
-    #st.write(prompt, probability_of_a_guess)
-    #quality_of_a_guess = compute_entropy_of_a_guess(float(probability_of_a_guess))
-    #return quality_of_a_guess
-
-
 st.title("Statistics page")
 
 
-if "client" not in st.session_state:
-    st.session_state.client = OpenAI(api_key="sk-svcacct-sasYDh93HtW8T-ZtXNCUElcOwmpB__D0ql2JJXLPl3kTrwrVeY2W_hTXl1AhYMsT3BlbkFJLcW4LbU2SOAgDFeOFXJyCA-l_xvOKYqPDTy1YJ2lGsEqPHLIXGwctBw7FOuGVAA")
+#if "client" not in st.session_state:
+#    st.session_state.client = OpenAI(api_key="sk-svcacct-sasYDh93HtW8T-ZtXNCUElcOwmpB__D0ql2JJXLPl3kTrwrVeY2W_hTXl1AhYMsT3BlbkFJLcW4LbU2SOAgDFeOFXJyCA-l_xvOKYqPDTy1YJ2lGsEqPHLIXGwctBw7FOuGVAA")
   
 
-if "current_target_species_name" in st.session_state:
-    st.write (st.session_state.current_target_species_name)
+#if "current_target_species_name" in st.session_state:
+#    st.write (st.session_state.current_target_species_name)
 
+#compute and disply average number of guesses per iteration if there is at least one successful guess
 if 'total_number_of_guesses_per_game_iteration' in st.session_state and len(st.session_state.total_number_of_guesses_per_game_iteration) != 0:
     mean_guesses_per_game_iteration = sum(st.session_state.total_number_of_guesses_per_game_iteration) / len(st.session_state.total_number_of_guesses_per_game_iteration)
     st.write("Average number of guesses per finished game iteration: ", mean_guesses_per_game_iteration)
 
-    # Create a DataFrame with default index 
+    
+    # Create a DataFrame with stores the number of guesses per game iteration 
     number_of_guesses_per_game_iteration = pd.DataFrame(st.session_state.total_number_of_guesses_per_game_iteration, columns=['Values'])
 
-    # Create the bar chart
+    #display a that dataframe as a bar chart
     st.bar_chart(number_of_guesses_per_game_iteration)
+    
 
 
 
 
 if "history_of_guesses" in st.session_state:
 
+    st.write("""
+        Entropy of a guess is the metric chosen to quantify quality or goodness of a guess.
+        If a guess eliminates roughly a half of animal species irrespective of whether it is correct or not, then it is a good guess,
+        because one can use several such guesses to arrive at the target species quickly irrespective of what the target species is.
+        If a guess only eliminates a couple of species when correct or incorrect, then one might need lots of such guesses to be
+        guaranteed to arrivate at the target species  
+    """)
+    #store broader info on guesses in a dataframe
     broader_info_on_guesses = pd.DataFrame(st.session_state.history_of_guesses, columns=[
-        'Guess', 'Correct species name', 'Truth value of a guess', 'Number of guesses in this game iteration'
+        'Guess', 'Correct species name', 'Truth value of a guess', 'Guess per iteration', 'Estimated guess quality'
     ])
 
-    st.write(st.session_state.species_info_dataframe["Class"].value_counts())
+    #display that dataframe
     st.write(broader_info_on_guesses)
-    st.write(broader_info_on_guesses['Guess'].apply(estimate_quality_of_a_guess))
+  
